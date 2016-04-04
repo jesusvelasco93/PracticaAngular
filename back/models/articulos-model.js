6b1ba3e23cf3c538c145523658bf4ecde8767363
Jesus Velasco <jesus.velasco@babel.es>
2016-04-04 14:01:07 +0200

    Falta hacer post de anuncios y comprobar que los get funcionan, despues pasar a Front

---
+++ back/models/articulos-model.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// Conectar con driver
+// Conecctar con la base de datos
+
+require('../lib/connectMongoose');
+var mongoose = require('mongoose');
+
+// Creo el esquema
+var articuloSchema = mongoose.Schema({
+    title: String,
+    url_video: String,
+    url_imagen: String,
+    overview: String,
+    rating: Number,
+});
+
+articuloSchema.statics.list = function(cb){
+
+    // Preparamos la Query sin ejecutarla (No ponemos callback a find)
+    // Hacemos la busqueda con los parametros finales
+    var query = Articulo.find({});
+
+    // Añadimos mas parámetros a la query
+
+    // La ejecutamos
+    query.exec(function(err, articles) {
+
+        // Devolvemos error
+        if (err) {
+            cb(err);
+            return;
+        }
+        // var numElemMostrados = rows.length;
+
+        // O devolvemos los resultados
+        cb(null, articles);
+    });
+};
+
+anuncioSchema.statics.item = function(title, cb){
+
+    // Preparamos la Query sin ejecutarla (No ponemos callback a find)
+    var query = Articulo.find({"title": title});
+
+    // La ejecutamos
+    query.exec(function(err, article){
+
+        // Devolvemos error
+        if (err){
+            cb(err);
+            return;
+        }
+
+        // Devolvemos los resultados
+        cb(null, article);
+    });
+};
+
+// Lo registro en mongoose
+var Articulo = mongoose.model('Articulos', articuloSchema);
--- back/routes/articles.js
+++ back/routes/articles.js
@@ -1,9 +1,44 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Anuncio = mongoose.model('Anuncios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('articles', { articles: 'Articulo' });
+// Get anuncios listing
+router.get('/', function(req, res) {
+
+    title = req.header.title || "";
+    if (title == "") {
+        // Llamamos a la busqueda con estos parametros y se lo devolvemos o renderizamos a la vista
+        Anuncio.list(function(err, articles) {
+
+            if (err) {
+                res.json({ anuncios: "", err: err });
+                return;
+            }
+
+            // res.json({ result: true, anuncios: rows });
+            res.json({ anuncios: articles, err: "" });
+        });
+    }
+    else {
+        Anuncio.item(title, function(err, article) {
+
+            if (err) {
+                res.json({ anuncio: "", err: err });
+                return;
+            }
+
+            // res.json({ result: true, anuncios: rows });
+            res.json({ anuncio: artcicle, err: "" });
+        });
+    }
+});
+
+
+router.post('/new', function(req, res) {
+  // FALTA HACER POST
 });
 
 module.exports = router;
